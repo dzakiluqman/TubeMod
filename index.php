60f7bf0b645476aae5ea011f8e7585840e20322f
dzakiluqman <dzakiluqman275@gmail.com>
2026-04-06 02:21:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (!session_id()) {
     session_start();
 }
 
-// require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 require_once 'app/config/config.php';
 require_once 'app/core/App.php';
 require_once 'app/core/Controller.php';
