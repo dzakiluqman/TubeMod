54c9a7e5bc8c3f3b588da7e4db26d6f9ac1690a0
dzakiluqman <dzakiluqman275@gmail.com>
2026-04-08 20:46:10 +0700

    Remove Button Login Debug

--- app/views/login.php
+++ app/views/login.php
@@ -9,10 +9,6 @@
             <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="Google">
             Continue with Google
         </a>
-        
-        <form method="POST" action="<?= BASEURL ?>/auth/login">
-            <button name="login" class="hidden-login">bypass login</button>
-        </form>
     </div>
 </div>
 
